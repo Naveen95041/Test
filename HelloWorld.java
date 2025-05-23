9bc764adbe6d88102c8a20c7f34bb16cd79c8cc8
root <root@ip-172-31-95-83.ec2.internal>
2025-05-23 14:11:49 +0000

    Tea

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! naveen MIshra abc");
+        System.out.println("Hello, World! naveen MIshra abc test");
     }
 }
 
