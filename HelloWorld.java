914a5d6bc8f7b720801d201e8d469257c6543cc5
root <root@ip-172-31-82-78.ec2.internal>
2025-05-24 06:49:48 +0000

    Done me22

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! Welome to Jenkinssssss:");
+        System.out.println("Hello, World! Welome to Jenkinssssss:Naveen");
     }
 }
 
