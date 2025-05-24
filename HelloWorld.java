e8eca7f358722982c79ebf0510f8190b561b68dc
root <root@ip-172-31-82-78.ec2.internal>
2025-05-24 06:40:21 +0000

    new message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! naveen MIshra abc test NIbha Sharma NIbha Mishra");
+        System.out.println("Hello, World! Welome to Jenkins");
     }
 }
 
