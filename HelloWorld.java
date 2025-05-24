ad5278c7ed41fb6743274d797522e5ff5a708246
root <root@ip-172-31-82-62.ec2.internal>
2025-05-24 10:15:18 +0000

    Webhook

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! Welome to Jenkinssssss:Naveen shekhar yadav abc dd ram ja rha hai")
+        System.out.println("Hello, World! Welome to Jenkins WebHook")
     }
 }
 
