3e9c77ce89ef53a7f1e22da12fcd08d5a4bff60c
root <root@ip-172-31-82-62.ec2.internal>
2025-05-24 09:33:58 +0000

    Hi Ram

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! Welome to Jenkinssssss:Naveen shekhar");
+        System.out.println("Hello, World! Welome to Jenkinssssss:Naveen shekhar yadav")
     }
 }
 
